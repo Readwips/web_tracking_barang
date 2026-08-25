fa6dcaa71a2125f3d2bafdc5351b7294e486e7d7
Rea <hinakosakhiri@gmail.com>
2026-08-25 12:25:53 +0700

    build: setup Vercel serverless deployment configuration

---
+++ api/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Vercel Serverless Entry Point
+ * This acts as the entry point for Vercel functions, mapping to Laravel's public/index.php
+ */
+
+require __DIR__ . '/../public/index.php';
