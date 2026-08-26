742abdbe7f24219238d92b1dcdd0f32208eaabc6
Rea <hinakosakhiri@gmail.com>
2026-08-26 16:01:08 +0700

    fix: support Laravel runtime on Vercel

--- api/index.php
+++ api/index.php
@@ -1,8 +1,9 @@
 <?php
 
-/**
- * Vercel Serverless Entry Point
- * This acts as the entry point for Vercel functions, mapping to Laravel's public/index.php
- */
+foreach (['/tmp/views', '/tmp/bootstrap/cache', '/tmp/framework/cache', '/tmp/framework/sessions'] as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0777, true);
+    }
+}
 
-require __DIR__ . '/../public/index.php';
+require __DIR__.'/../public/index.php';
